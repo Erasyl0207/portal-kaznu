3bf67752596ea27d01e3bb96e67a275a032a04f1
Erasyl0207 <kantoreuly02@gmail.com>
2022-11-28 02:02:54 +0600

    Initial version of portal_kaznu generated by generator-jhipster@7.9.3

---
+++ src/main/java/kz/kaznu/portal/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package kz.kaznu.portal.service.mapper;
---
+++ src/main/java/kz/kaznu/portal/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package kz.kaznu.portal.web.rest.vm;
